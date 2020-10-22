1abcb59b9dbd18f33b3f8cc84cd7de6a2acf466d
juan.vilata@iteriam.es <juan.vilata@iteriam.es>
2020-10-22 17:46:41 +0200

    Organize import

--- calculator/src/main/java/com/iteriam/calculator/exception/AppExceptionHandler.java
+++ calculator/src/main/java/com/iteriam/calculator/exception/AppExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
