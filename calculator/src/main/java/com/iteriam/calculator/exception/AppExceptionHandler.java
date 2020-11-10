53dc059c9383c0311e7d3642564accddfa5ea9d1
juan.vilata@iteriam.es <juan.vilata@iteriam.es>
2020-11-10 16:29:09 +0100

    Modify Control Exception

--- calculator/src/main/java/com/iteriam/calculator/exception/AppExceptionHandler.java
+++ calculator/src/main/java/com/iteriam/calculator/exception/AppExceptionHandler.java
@@ -1,12 +1,8 @@
 package com.iteriam.calculator.exception;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,9 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
